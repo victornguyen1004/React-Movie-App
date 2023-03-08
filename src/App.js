216da3a8a4295f768d0b86bf46cda2d06cb58a04
victornguyen1004 <quanggvuu1004@gmail.com>
2023-03-09 03:30:19 +0700

    change api url

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import SearchIcon from "./search.svg";
 
 import MovieCard from "./MovieCard";
 
-const API_URL = "http://www.omdbapi.com?apikey=38bcb277";
+const API_URL = "https://www.omdbapi.com?apikey=38bcb277";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
